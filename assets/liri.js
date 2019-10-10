09e96c43bed4996c5ebae3b16da108c2aaa1a2ef
Eric Cadwell <ericmcadwell@gmail.com>
2019-10-09 18:59:59 -0700

    fixes

--- assets/liri.js
+++ assets/liri.js
@@ -8,31 +8,25 @@ var fs = require('fs');
 var spotify = new Spotify(keys.spotify);
 var argument = process.argv[2];
 var stringArgv = process.argv;
-var artist = "";
-var userSong = "";
-var userMovie = "";
+var userInput = "";
 
-var startApp = function(argument) {
+// var startApp = function(argument) {
     for (var i = 3; i < stringArgv.length; i++) {
         if (i > 3 && i < stringArgv.length) {
-            artist = artist + " " + stringArgv[i];
-            userSong = userSong + " " + stringArgv[i];
-            userMovie = userMovie + "+" + stringArgv[i];
+            userInput =userInput + " " + stringArgv[i];
         } else {
-            artist += stringArgv[i];
-            userSong += stringArgv[i];
-            userMovie += stringArgv[i];
+            userInput += stringArgv[i];
         }
     }
 switch (argument) {
     case "concert-this":
-        concertThis();
+        concertThis(userInput);
         break;
     case "spotify-this-song":
-        spotifyThisSong();
+        spotifyThisSong(userInput);
         break;
     case "movie-this":
-        movieThis();
+        movieThis(userInput);
         break;
     case "do-what-it-says":
         doWhatItSays();
@@ -45,23 +39,22 @@ switch (argument) {
         break;
     default: console.log(argument); 
         break;
-}};
+};
 
 function hiThere(){
     console.log("Well Hello! Please access one of the following commands:");
     console.log("[Concert-This]-[Spotify-This-Song]-[Movie-This]-[Do-What-It-Says]");
 }
 
-function concertThis() {
-    console.log("toastadas");
+function concertThis(artist) {
+    // console.log("toastadas");
     if (artist === "") {
-        artist="Ted Nugent";
+        artist="Post Malone";
     }       
     var getBiT = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
     axios.get(getBiT).then(
         function (response, err) {
             if (response) {
-                console.log(response.data[0]);
                 var infoBiT = response.data[0];
                 var venueName = "Venue Name: " + infoBiT.venue.name;
                 var venueLocation = "Venue Location: " + infoBiT.venue.city;
@@ -84,9 +77,9 @@ function concertThis() {
         }); 
 }
 
-function spotifyThisSong() {
+function spotifyThisSong(userSong) {
     if (userSong === "") {
-        userSong = "The Sign Ace of Base";}
+        userSong = "Circles";}
         {
         spotify.search({
         type: 'track',
@@ -114,9 +107,9 @@ function spotifyThisSong() {
 }
 }
 
-function movieThis() {
+function movieThis(userMovie) {
     if (userMovie === "") {
-    userMovie="Batman";}
+    userMovie="Interstellar";}
     var queryUrl = "http://www.omdbapi.com/?t=" + userMovie + "&y=&plot=short&apikey=trilogy";
 
     console.log(queryUrl);
@@ -124,45 +117,16 @@ function movieThis() {
     axios.get(queryUrl).then(function (response, err) {
         console.log(err ? 'Errors occurred: ' + err : "");
         if (response) {
-            var infoA = response;
-            console.log(infoA);
-            console.log("Title: " + response.Title);
-            console.log("Release Year: " + JSON.parse(data).Year);
-            console.log("Rated: " + JSON.parse(this).Rated);
-            console.log("Actors: " + JSON.parse(body).Actors);
-            console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
-            console.log("Rotten Tomatoes Rating: " + JSON.parse(body).rottenTomatoesRating); 
-            console.log("Country/Countries Filmed: " + JSON.parse(body).Country);
-            console.log("Language(s): " + JSON.parse(body).Language);
-            console.log("Plot: " + JSON.parse(body).Plot);
-        }else{                
-            console.log(err);
-        }
-    });
-}
-
-function movieThis() {
-    if (userMovie === "") {
-    userMovie="Mr.+Nobody";}
-    var queryUrl = "http://www.omdbapi.com/?t=" + userMovie + "&y=&plot=short&apikey=trilogy";
-
-    console.log(queryUrl);
-
-    axios.get(queryUrl).then(function (response, err) {
-       
-        console.log(err ? 'Errors occurred: ' + err : "");
-        if (response) {
-          
-            console.log("Retrieving movie information for " + userSong);
-            console.log("Title: " + response.data.Title);
-            console.log("Release Year: " + response.data.Year);
-            console.log("Rated: " + response.data.Rated);
-            console.log("Actors: " + response.data.Actors);
-            console.log("IMDB Rating: " + response.data.imdbRating);
-            console.log("Rotten Tomatoes Rating: " + response.data.rottenTomatoesRating);
-            console.log("Country/Countries Filmed: " + response.data.Country);
-            console.log("Language(s): " + response.data.Language);
-            console.log("Plot: " + response.data.Plot);
+            var infoA = response.data;
+            console.log("Title: " + infoA.Title);
+            console.log("Release Year: " + infoA.Year);
+            console.log("Rated: " + infoA.Rated);
+            console.log("Actors: " + infoA.Actors);
+            console.log("IMDB Rating: " + infoA.imdbRating);
+            console.log("Rotten Tomatoes Rating: " + infoA.rottenTomatoesRating); 
+            console.log("Country/Countries Filmed: " + infoA.Country);
+            console.log("Language(s): " + infoA.Language);
+            console.log("Plot: " + infoA.Plot);
         }else{                
             console.log(err);
         }
@@ -176,10 +140,31 @@ function doWhatItSays() {
 		}else{
 		var dataArr = data.split(",");
 		action = dataArr[0];
-        songTitle = dataArr[1];
-        userSong = action + "" + songTitle;
-		spotifyThisSong();
+        userInput = dataArr[1];
+        userSong = action + "" + userInput;
+		switch (action) {
+            case "concert-this":
+                concertThis(userInput);
+                break;
+            case "spotify-this-song":
+                spotifyThisSong(userInput);
+                break;
+            case "movie-this":
+                movieThis(userInput);
+                break;
+            case "do-what-it-says":
+                doWhatItSays();
+                break;
+            case "hi":
+                hiThere();
+                break;
+            case "help":
+                hiThere();
+                break;
+            default: console.log(argument); 
+                break;
+        };
         }
     }); 
 }
-startApp();
+// startApp();
